4d4b7972e265acc5bf3f3ee41ea4c67022565bc4
flexflower <kelyancsajka@gmail.com>
2023-04-18 06:20:12 +0200

    Updates

---
+++ electron.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+    process.env.ELECTRON_START_URL = process.env.ELECTRON_START_URL || `http://localhost:3030`;
+    const startUrl =
+        process.env.ELECTRON_START_URL ||
+        url.format({
+            pathname: path.join(__dirname, '/../build/index.html'),
+            protocol: 'file:',
+            slashes: true,
+        });
+
+    mainWindow.loadURL(startUrl);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
